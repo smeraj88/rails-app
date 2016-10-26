d2a9cacaa510c0c1dc3cdb96b110fcf3bc386c1f
smeraj <smeraj231@gmail.com>
2016-10-26 06:38:44 +0000

    hello app inserted

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+      render html: "hello , world!"
+    end
+    
+     
+  def goodby
+      render html: "Ihola, mundo!"
+      #"goodby , world!"
+    end
+    
+end
